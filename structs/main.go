5a7dbad9a6d5ca9236e0cb84aa33512f5afb57d7
Xin Zhe Khooi <khooi8913@gmail.com>
2021-05-17 23:12:55 +0800

    updated structs

--- structs/main.go
+++ structs/main.go
@@ -50,3 +50,26 @@ func (pointerToPerson *person) updateName(newFirstName string) {
 func (p person) print() {
 	fmt.Printf("%+v", p)
 }
+
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mySlice := []string {
+	"Hi",
+	"There",
+	"How",
+	"Are",
+	"You"}
+	updateSlice(mySlice)
+	fmt.Println(mySlice)
+}
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+}
+*/
